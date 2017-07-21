74b3f21afc359c000e6d0a08239976456d9e6645
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-07-21 08:53:23 -0700

    Functioning Manager

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ## bamazon-rm
-### bamazon homework week 7 Instructions for customers:
+### bamazon homework week 7 Instructions for Customers:
 
 1. Run the schema.sql file to start the database.
 2. User should open Terminal and navigate to where this repo was cloned.
 3. In Terminal, type in "npm install" to install all of the packages needed.
-4. Once installed, type in "node bamazonCustomer.js" to begin the CLI application.
+4. Once installed, type in "node bamazonCustomer.js" to begin the CLI application for customers.
 5. A table should populate in your Terminal window. If the table looks broken, resize your Terminal window to accommodate the entire table.
 6. The User is prompted to type in the item id number they want to purchase, then the quantity they want to purchase.
 7. If the quantity the user wants to purchase is more than the quantity in stock, a message displays explaining that the purchase can't proceed, then redirects the User to try again.
@@ -13,4 +13,14 @@
 
 Link to download the video for the basicCustomer.js (https://github.com/Ropestar1/bamazon-rm/blob/master/walkthroughs/bamazonCustomer-rm.mov)
 
+### bamazon homework week 7 Instructions for Managers:
 
+1. See steps 1-3 In bamazon Customer section for initial setup.
+2. In Terminal, type in "node bamazonManager.js" to begin the CLI application for managers.
+3. Chooose what you want to do from the list:
+..* View Products for Sale: Displays a table with items for sale.
+..* View Low Inventory: Displays a table showing inventory with less than 5 stock quantity.
+..* Add to Inventory: Allows Manager to add to inventory of existing products for sale.
+..* Add New Product: Adds a new product to the items available for sale.
+
+Link to download the video for the basicManager.js ()
--- bamazonManager.js
+++ bamazonManager.js
@@ -105,15 +105,54 @@ function addInventoryView() {
   });
 }
 
-function addNewProduct () {
-
+function addNewProduct() {
+  // prompt for info about the item being put up for auction
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What item would you like to add?",
+        name: "productName"
+      },
+      {
+        type: "input",
+        message: "What department does it belong in?",
+        name: "departmentName"
+      },
+      {
+        type: "input",
+        message: "How much does the item cost? Round to the nearest dollar.",
+        name: "priceInput"
+      },
+      {
+        type: "input",
+        message: "How many units are in stock?",
+        name: "stockInput"
+      },
+    ])
+    .then(function(answer) {
+      connection.query(
+        "INSERT INTO products SET ?",
+        {
+          product_name: answer.productName,
+          department_name: answer.departmentName,
+          price: answer.priceInput,
+          stock_quantity: answer.stockInput
+        },
+        function(err) {
+          if (err) throw err;
+          console.log("Your auction item was added successfully.");
+          managerPrompts();
+        }
+      );
+    });
 }
 
 function managerPrompts() {
   var productMessage = 'View Products for Sale';
   var lowInventoryMessage = 'View Low Inventory';
   var addInventoryMessage = 'Add to Inventory';
-  var addNewProductMessage = 'Add new product.';
+  var addNewProductMessage = 'Add New Product.';
 
   inquirer.prompt([
     { 
@@ -129,52 +168,6 @@ function managerPrompts() {
 
     else if (answer.action === addInventoryMessage) addInventoryView();
 
-    // else addNewProduct(); 
-
-    // var query = "SELECT * FROM products WHERE ?";
-    // connection.query(query, { item_id: answer.productID }, function(err, res) {
-    //   if (err) throw err;
-
-    //   if (parseInt(answer.quantityToBuy) > res[0].stock_quantity) {
-    //     console.log('I\'m sorry, but we don\'t have that many in stock. Please ' +
-    //     'select a new amount less than the Stock Qty.');
-    //     managerPrompts();
-    //   }
-    //   else {
-    //     var itemTable = new Table({
-    //       head: ['Item ID', 'Product', 'Department', 'Price', 'Stock Qty'],
-    //       colWidths: [10, 35, 30, 10, 12]
-    //     });
-
-    //     connection.query(
-    //       "UPDATE products SET ? WHERE ?",
-    //       [
-    //         {
-    //           stock_quantity: res[0].stock_quantity - parseInt(answer.quantityToBuy)
-    //         },
-    //         {
-    //           item_id: answer.productID
-    //         }
-    //       ],
-    //       function(error) {
-    //         if (error) throw error;
-    //         var totalCost = answer.quantityToBuy * res[0].price;
-
-    //         console.log("Item purchased successfully!");
-    //         connection.query(query, { item_id: answer.productID }, function(err, res) {
-    //           if (err) throw err;
-    //           itemTable.push(
-    //           [res[0].item_id, res[0].product_name, res[0].department_name,
-    //           res[0].price, res[0].stock_quantity]);
-
-    //           console.log(itemTable.toString());
-    //           console.log('You\'re total comes out to be $' + totalCost.toString())
-    //           queryProducts();
-    //           // connection.end();
-    //         });
-    //       }
-    //     );
-    //   }      
-    // });
+    else addNewProduct();
   });
 }
