ed41e04df3f690bcf0fdb2dd287c24137108ae67
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-07-20 23:13:59 -0700

    read me

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# bamazon-rm
-bamazon homework week 7
+## bamazon-rm
+#####bamazon homework week 7 Instructions for customers:
+
+1. User should open Terminal and navigate to where this repo was cloned.
+2. In Terminal, type in "npm install" to install all of the packages needed.
+3. Once installed, type in "node bamazonCustomer.js" to begin the CLI application.
+4. A table should populate in your Terminal window. If the table looks broken, resize your Terminal window to accommodate the entire table.
+5. The User is prompted to type in the item id number they want to purchase, then the quantity they want to purchase.
+6. If the quantity the user wants to purchase is more than the quantity in stock, a message displays explaining that the purchase can't proceed, then redirects the User to try again.
+7. If the quantity the user wants to purchase is below the stock quantity, the program deducts the User's purchase from the stock quantity and displays the updated information and the total cost of the transaction.
+8. In order to exit the application, press CTRL-C.
+
+Link to the video for the basicCustomer.js ()
+
+
---
+++ bamazonManager.js
@@ -0,0 +1,99 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+// var colors = require('colors');
+var Table = require('cli-table');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'Noix72NfyvPJdkQq',
+  database: 'bamazon'
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  // console.log(connection)
+  console.log('connected as id ' + connection.threadId);
+  queryProducts();
+});
+
+//function library
+function queryProducts() {
+  connection.query('SELECT * FROM products', function(err, res) {
+    var table = new Table({
+      head: ['Item ID', 'Product', 'Department', 'Price', 'Stock Qty'],
+      colWidths: [10, 35, 30, 10, 12]
+    });
+
+    for (var i = 0; i < res.length; i++) {
+      table.push(
+        [res[i].item_id, res[i].product_name, res[i].department_name,
+        res[i].price, res[i].stock_quantity]);
+    }
+
+    console.log(table.toString());
+    customerPrompts();
+  });
+}
+
+function customerPrompts() {
+  inquirer.prompt([
+    { 
+			type: 'input', //default type is input if type left out
+			message: 'What product item would you like to purchase? Please specify Item ID',
+			name: 'productID'
+    },
+    { 
+      type: 'input', //default type is input if type left out
+      message: 'How many would you like to purchase?',
+      name: 'quantityToBuy'
+    }
+	]).then(function(answer) {
+    var query = "SELECT * FROM products WHERE ?";
+    connection.query(query, { item_id: answer.productID }, function(err, res) {
+      if (err) throw err;
+
+      if (parseInt(answer.quantityToBuy) > res[0].stock_quantity) {
+        console.log('I\'m sorry, but we don\'t have that many in stock. Please ' +
+        'select a new amount less than the Stock Qty.');
+        customerPrompts();
+      }
+      else {
+        var itemTable = new Table({
+          head: ['Item ID', 'Product', 'Department', 'Price', 'Stock Qty'],
+          colWidths: [10, 35, 30, 10, 12]
+        });
+
+        connection.query(
+          "UPDATE products SET ? WHERE ?",
+          [
+            {
+              stock_quantity: res[0].stock_quantity - parseInt(answer.quantityToBuy)
+            },
+            {
+              item_id: answer.productID
+            }
+          ],
+          function(error) {
+            if (error) throw error;
+            var totalCost = answer.quantityToBuy * res[0].price;
+
+            console.log("Item purchased successfully!");
+            connection.query(query, { item_id: answer.productID }, function(err, res) {
+              if (err) throw err;
+              itemTable.push(
+              [res[0].item_id, res[0].product_name, res[0].department_name,
+              res[0].price, res[0].stock_quantity]);
+
+              console.log(itemTable.toString());
+              console.log('You\'re total comes out to be $' + totalCost.toString())
+              queryProducts();
+              // connection.end();
+            });
+          }
+        );
+      }      
+    });
+  });
+}
